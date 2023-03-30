b7422d1ac7cbd354ea84abf0788971ce57518afa
AgnesG19 <agnesgm19@gmail.com>
2023-03-30 11:04:51 +0200

    Classe creada para establecer Conexion con BD en SQL Server.

---
+++ GenteFit/BBDD/ConexionBD.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace GenteFit.BBDD
+{
+    internal class ConexionBD{
+
+        private SqlConnection BD;
+
+        public ConexionBD()
+        {
+            this.BD = new SqlConnection("Server=Franky-PC\\NET ; DataBase=GenteFITBD");
+        }
+
+        public bool Conectar()
+        {
+            try
+            {
+                this.BD.Open();
+                return true;
+            }catch(SqlException ex)
+            {
+                return false;
+            }
+        }
+        public void Desconectar()
+        {
+            this.BD.Close();
+        }
+
+        private void ImprimirEstadoConexion()
+        {
+            Console.WriteLine("El estado de la conexión es: " + this.BD.State.ToString());
+        }
+    }
+}
